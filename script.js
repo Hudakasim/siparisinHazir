ca6c247bf8dd7f2db9a9b0a3979b77d3fd37d4b1
Fatma Cicek <fatmacicek139@gmail.com>
2025-03-23 15:47:18 +0300

    fatma

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(type) {
+    document.getElementById(type + "Modal").style.display = "flex";
+  }
+  
+  function closeModal(type) {
+    document.getElementById(type + "Modal").style.display = "none";
+  }
+  
