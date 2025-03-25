7b448f7151c785d40dabbc016746a788fb743cb5
Fatma Cicek <fatmacicek139@gmail.com>
2025-03-26 00:02:39 +0300

    iletisimformuyapildi

--- anaSayfa/script.js
+++ anaSayfa/script.js
@@ -1,3 +1,15 @@
 function goToMenu(page) {
   window.location.href = page;
 }
+const inputs = document.querySelectorAll(".input");
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    input.parentNode.classList.add("focus");
+  });
+  input.addEventListener("blur", () => {
+    if (input.value === "") {
+      input.parentNode.classList.remove("focus");
+    }
+  });
+});
