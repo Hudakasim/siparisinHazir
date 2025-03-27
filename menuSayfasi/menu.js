c0cbfc178a82939418dca7509ddd3cc86c8a2f95
hudakasim <030722088@std.izu.edu.tr>
2025-03-28 01:10:41 +0300

    sepete ekle dugmesi eklemeye calistim

--- menuSayfasi/menu.js
+++ menuSayfasi/menu.js
@@ -1,3 +1,31 @@
+function toggleCartButtons() {
+	// "Sepete Ekle" butonunu gizle
+	const addToCartButton = document.querySelector(".add-to-cart");
+	addToCartButton.style.display = "none";
+
+	// Artı/eksi butonlarını göster
+	const quantityControl = document.querySelector(".quantity-control");
+	quantityControl.style.display = "flex";
+  }
+
+  function updateQuantity(amount) {
+	const quantitySpan = document.getElementById("quantity");
+	let quantity = parseInt(quantitySpan.innerText);
+
+	// Yeni miktarı hesapla
+	quantity += amount;
+
+	// Sayı 0'a düşerse artı/eksi butonlarını gizle ve "Sepete Ekle"yi tekrar göster
+	if (quantity <= 0) {
+	  quantity = 1;
+	  document.querySelector(".quantity-control").style.display = "none";
+	  document.querySelector(".add-to-cart").style.display = "flex";
+	}
+
+	// Miktarı güncelle
+	quantitySpan.innerText = quantity;
+  }
+
 function showMenu(menuId) {
 	// tüm menüleri gizle
 	const sections = document.querySelectorAll('.menu-section');
