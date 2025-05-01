cacd28b848c8f938f6937fcfc9e2297db64dcbe9
BeyzaGuvercin101 <yazbeyzay@gmail.com>
2025-05-01 16:09:28 +0300

    yorum

--- backend/src/main/java/com/siparisinhazir/demo/controller/UserController.java
+++ backend/src/main/java/com/siparisinhazir/demo/controller/UserController.java
@@ -4,7 +4,9 @@ import com.siparisinhazir.demo.dto.LoginRequest;
 import com.siparisinhazir.demo.dto.RegisterRequest;
 import com.siparisinhazir.demo.dto.UserResponse;
 import com.siparisinhazir.demo.service.UserService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,17 +15,25 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+    private final ThreadLocal<UserService> userService = new ThreadLocal<>();
+    @Setter
+    @Getter
+    private LoginRequest loginRequest;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request) {
-        UserResponse registeredUser = userService.registerUser(request);
+        UserResponse registeredUser = getUserService().registerUser(request);
         return ResponseEntity.ok(registeredUser);
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        this.loginRequest = request;
         // Şimdilik sadece "başarılı giriş" mesajı dönelim
         return ResponseEntity.ok("Giriş başarılı (dummy)");
+    }
+
+    public UserService getUserService() {
+        return userService.get();
     }
 }
